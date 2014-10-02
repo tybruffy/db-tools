3ac45b528194946b697bbba4a15f91d8601782c5
Tyler Bruffy <TyBruffy@gmail.com>
2014-10-02 15:34:07 -0400

    env files are now json format

--- load_environment.php
+++ load_environment.php
@@ -1,17 +1,14 @@
 <?php
-if(file_exists(__DIR__ . "/production_env.php")) {
-	require_once(__DIR__ . "/production_env.php");
-} elseif(file_exists(__DIR__ . "/staging_env.php")) {
-	require_once(__DIR__ . "/staging_env.php");
-} elseif(is_file(__DIR__ . "/development_env.php")) {
-	require_once(__DIR__ . "/development_env.php");
-} elseif(is_file("../../environment/production_env.php")) { // For deployment via Capistrano
-	require_once("../../environment/production_env.php"); // For deployment via Capistrano
-} elseif(is_file("../../environment/staging_env.php")) { // For deployment via Capistrano
-	require_once("../../environment/staging_env.php"); // For deployment via Capistrano
-} elseif(is_file("../../environment/development_env.php")) { // For deployment via Capistrano
-	require_once("../../environment/development_env.php"); // For deployment via Capistrano
-} else {
-	die("Sorry! We're busy making this site better. We'll be back online shortly. (No environment could be loaded.)");
-}	
-?>
+	
+	if ( is_file(__DIR__ . "/production_env.json") ) {
+		$json = file_get_contents(__DIR__ . "/production_env.json");
+	} elseif( is_file(__DIR__ . "/staging_env.json") ) {
+		$json = file_get_contents(__DIR__ . "/staging_env.json");
+	} elseif( is_file(__DIR__ . "/development_env.json") ) {
+		$json = file_get_contents(__DIR__ . "/development_env.json");
+	} else {
+		die("(No environment file could be loaded.)");
+	}
+
+	$data = json_decode($json, true);
+	$_ENV = array_merge($_ENV, $data);
